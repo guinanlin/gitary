e427a1ee41f9ed3fdf6004329b14a928b6ca78a1
guinan.lin <guinan.lin@foxmail.com>
2025-12-10 15:02:46 +0800

    fix: change allowedHosts configuration to true in Vite config

--- apps/web/config/vite.config.ts
+++ apps/web/config/vite.config.ts
@@ -113,6 +113,6 @@ export default defineConfig({
     host: "0.0.0.0",
     port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
     strictPort: false,
-    allowedHosts: "all",
+    allowedHosts: true,
   },
 });
